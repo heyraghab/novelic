660e725934a572b1a2075fd349a4226bd9ade67a
raghav <143791708+heyraghab@users.noreply.github.com>
2023-10-11 16:28:16 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/global.css';
 import localFont from 'next/font/local'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const myFont = localFont({ src: '../public/font2.otf' })
 
 export default function App({ Component, pageProps }) {
   return (
     <div className={myFont.className}>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </div>
   )
